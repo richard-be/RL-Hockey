4fd3fdeb09a19b51a78db53e3a6f5b605a67c0c7
richard-be <belowrichard@web.de>
2026-01-08 13:19:27 +0100

    tested run test, save state dict from actor for safety reasons

--- sac/run_test.py
+++ sac/run_test.py
@@ -11,6 +11,8 @@ import src.hockey_env as h_env
 from dataclasses import dataclass
 import os
 
+torch.serialization.add_safe_globals([Actor])
+
 
 def make_env(env_id, seed, weak_opponent, env_mode, opponent):
     print(env_id)
@@ -44,11 +46,19 @@ class Args:
     """the torch file from where to load the actor network"""
     opponent_file: str=""
     """the torch file from where to load the opponent actor network"""
+    env_mode: str="NORMAL"
+    """env play mode"""
+    torch_deterministic: bool = True
+    """if toggled, `torch.backends.cudnn.deterministic=False`"""
+    cuda: bool = False
+    """if toggled, cuda will be enabled by default"""
+    render: bool = True
+    """if toggled, window will be rendered"""
 
 if __name__ == "__main__":
 
     args = tyro.cli(Args)
-    run_name = f"{args.exp_name}__{args.weak_opponent}__{args.actor}__{int(time.time())}"
+    run_name = f"{args.exp_name}__{args.weak_opponent}__{args.actor_file}__{int(time.time())}"
 
     # TRY NOT TO MODIFY: seeding
     random.seed(args.seed)
@@ -57,17 +67,19 @@ if __name__ == "__main__":
     torch.backends.cudnn.deterministic = args.torch_deterministic
 
     device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
-
+    print(device)
     opponent = "Basic"
     if len(args.opponent_file) > 0:
-        opponent = torch.load(os.path.join("opponents"), args.opponent_file)
+        opponent = torch.load(os.path.join("opponents"), args.opponent_file, map_location=device) #also state dict load?
     # env setup
     envs = gym.vector.SyncVectorEnv(
         [make_env(args.env_id, args.seed + i, args.weak_opponent, args.env_mode, opponent) for i in range(args.num_envs)]
     )
     assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
 
-    actor = torch.load(os.path.join("actors", args.actor_file))
+    actor = Actor(envs)
+    actor.load_state_dict(torch.load(os.path.join("actors", args.actor_file)))
+    actor.to(device)
 
     start_time = time.time()
 
@@ -75,20 +87,20 @@ if __name__ == "__main__":
     obs, _ = envs.reset(seed=args.seed)
     episode_count = 0
     global_step = 0
-    while episode_count > args.num_games:
+    while episode_count < args.num_games:
         # ALGO LOGIC: put action logic here
         actions = np.array([envs.single_action_space.sample() for _ in range(envs.num_envs)])
-       
 
         # TRY NOT TO MODIFY: execute the game and log data.
         next_obs, rewards, terminations, truncations, infos = envs.step(actions)
 
-        print(infos["winner"])
-        #envs.envs[0].render()
+        if args.render:
+            envs.envs[0].render()
         # TRY NOT TO MODIFY: record rewards for plotting purposes
         if "final_info" in infos:
-            for info in infos["final_info"]:
+            for env_index, info in enumerate(infos["final_info"]):
                 if info is not None:
+                    print(infos["final_info"][env_index]["winner"])
                     episode_count += 1
                     if episode_count % 50 == 0:
                         print(f"episode={episode_count}, global_step={global_step}, episodic_return={info['episode']['r']}, episode_length={info['episode']['l']}")
