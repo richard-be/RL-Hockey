41a247cef375535cd1c43a2b5b42d346b3ee3413
Nargizi <ggoge19@freeuni.edu.ge>
2026-02-10 18:19:10 +0100

    update weight normalization logic

--- src/crossq/models/critic.py
+++ src/crossq/models/critic.py
@@ -3,10 +3,10 @@ from models.feedforward import FeedForward, NNConfig
 
 
 @torch.no_grad()
-def project_weight_to_norm_ball(W: torch.tensor, scale: float = 1.0):
-    n = torch.norm(W, p="fro")
+def project_weight_to_norm_ball(fc: torch.nn.Module, scale: float = 1.0):
+    n = fc.parametrizations.weight.original0.norm()
     if n > scale:
-        W.mul_(scale / (n + 1e-12))  # epsilon to avoid division by zero
+        fc.parametrizations.weight.original0.mul_(1 / (n + 1e-12))  # epsilon to avoid division by zero
 
 class QNetwork(FeedForward):
     def __init__(self, config: NNConfig, *args, **kwargs):
@@ -21,10 +21,15 @@ class QNetwork(FeedForward):
     # for Weight Normalization
 
     def normalize_weights_(self) -> None:
-        for name, parameter in self.named_parameters():
-            # only normalize non-finale dense layer's weights
-            if "body" in name and "dense" in name and "weight" in name:
-                project_weight_to_norm_ball(parameter)
+
+        for mod_name, mod in self.named_modules():
+            if ("body" in mod_name) and ("dense" in mod_name) and isinstance(mod, torch.nn.Linear):
+                project_weight_to_norm_ball(mod)
+
+        # for name, parameter in self.named_parameters():
+        #     # only normalize non-finale dense layer's weights
+        #     if "body" in name and "dense" in name and "weight" in name:
+        #         project_weight_to_norm_ball(parameter)
 
     def get_weight_norms(self) -> list[float]:
         norms = []
