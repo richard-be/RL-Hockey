510c4bbed8598f98e8369b8b844fc3333873401d
Nargizi <ggoge19@freeuni.edu.ge>
2026-02-14 17:38:11 +0100

    instead of unit norm keep norm to be near initialization

--- src/crossq/models/critic.py
+++ src/crossq/models/critic.py
@@ -7,7 +7,7 @@ from models.feedforward import FeedForward, NNConfig
 def project_weight_to_norm_ball(module: torch.nn.Linear, scale: float | None = None):
     weight, bias = module.weight, module.bias
     if not scale:
-        scale = math(module.weight.shape[-1] / 3)  # expected norm under pytorch's initialization U(- 1/ sqrt(fan_in), 1 / sqrt(fan_in))
+        scale = math.sqrt(module.weight.shape[-1] / 3)  # expected norm under pytorch's initialization U(- 1/ sqrt(fan_in), 1 / sqrt(fan_in))
     vec = torch.concat([weight.view(-1), bias.view(-1)])
     n = vec.norm(p=2)
     if scale < n:
